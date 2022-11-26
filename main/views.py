77434ea97366df67f912ab17e9d4e90cee5e8682
Sweet As KaraKum <76500727+SwAsKk@users.noreply.github.com>
2022-11-26 14:22:44 +0300

    Minimal functionality
    Added base,footer,header files
    -Modified index.html
    -Created function for generating passwords

--- main/views.py
+++ main/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-
+import random
 # Create your views here.
 
 def index(requests):
     context = {}
     context["site_name"] = "Password generator"
+
+    lower_case = "abcdefghijklmnopqrstuvwyz"
+    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWYZ"
+    digit = "1234567890"
+    symbols = "@#$%^&/\?"
+    Use_fir =lower_case+upper_case+digit+symbols
+
+    if requests.method == "POST":
+        size = int(requests.POST.get('size', 0))
+        password = "".join(random.sample(Use_fir,size))
+        context['password'] = password
+
     return render(requests, 'pages/index.html', context)
